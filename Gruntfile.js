4d344d90607a9d1e68f2a3c6d3d5b1a15eaae75d
Yan Gao <gao.yan.gg1994@gmail.com>
2019-04-11 22:34:50 -0400

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,51 @@
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-screeps');
+    var config = require('./.screeps.json')
+    var branch = grunt.option('branch') || config.branch;
+    var email = grunt.option('email') || config.email;
+    var password = grunt.option('password') || config.password;
+    var ptr = grunt.option('ptr') ? true : config.ptr
+
+    grunt.loadNpmTasks('grunt-screeps')
+    grunt.loadNpmTasks('grunt-contrib-clean')
+    grunt.loadNpmTasks('grunt-contrib-copy')
 
     grunt.initConfig({
         screeps: {
             options: {
-                email: 'gao.yan.gg1994@gmail.com',
-                password: '377L8cybsNSmKqU',
-                branch: 'default',
-                ptr: false
+                email: email,
+                password: password,
+                branch: branch,
+                ptr: ptr
             },
             dist: {
-                files: [
-                    {
-                        expand: true,
-                        cwd: 'dist/',
-                        src: ['**/*.{js,wasm}'],
-                        flatten: true
-                    }
-                ]
+                src: ['dist/*.js']
             }
-        }
-    });
+        },
+
+        // Remove all files from the dist folder.
+        clean: {
+          'dist': ['dist']
+        },
+
+        // Copy all source files into the dist folder, flattening the folder structure by converting path delimiters to underscores
+        copy: {
+          // Pushes the game code to the dist folder so it can be modified before being send to the screeps server.
+          screeps: {
+            files: [{
+              expand: true,
+              cwd: 'src/',
+              src: '**',
+              dest: 'dist/',
+              filter: 'isFile',
+              rename: function (dest, src) {
+                // Change the path name utilize underscores for folders
+                return dest + src.replace(/\//g,'_');
+              }
+            }],
+          }
+        },
+    })
+
+    grunt.registerTask('default',  ['clean', 'copy:screeps', 'screeps']);
 }
